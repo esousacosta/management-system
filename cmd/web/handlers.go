414acde01741780fafadd50ab8264b6da4876a66
edesousacosta <emanoel.desousacosta@amadeus.com>
2024-08-25 15:49:39 -0300

    Add more templates for homepage and fix paths for fs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Simplex management system!")
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"../../ui/html/base.html",
+		"../../ui/html/pages/home.html",
+		"../../ui/html/partials/nav.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) viewPart(w http.ResponseWriter, r *http.Request) {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func (app *application) route() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	fmt.Println(os.Getwd())
+	fileServer := http.FileServer(http.Dir("./../../ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/part/view", app.viewPart)
 	mux.HandleFunc("/part/create", app.createPart)
