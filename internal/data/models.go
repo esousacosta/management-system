b744da3f1e177fd7f5faf61223960210c5a26d02
Emanoel <victoremanoel23@gmail.com>
2024-08-26 18:42:47 -0300

    Modify application type to hold OrderModel

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,12 +3,16 @@ package data
 import "database/sql"
 
 type Models struct {
-	Parts PartModel
+	Parts  PartModel
+	Orders OrderModel
 }
 
 func NewModel(db *sql.DB) *Models {
 	return &Models{
 		Parts: PartModel{
+			db: db,
+		},
+		Orders: OrderModel{
 			db: db,
 		},
 	}
