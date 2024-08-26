460d128c4e3116c2df41cc6ba000129b0ad102b2
edesousacosta <emanoel.desousacosta@amadeus.com>
2024-08-26 10:43:24 -0300

    Add create part tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -74,6 +74,25 @@ func (app *application) viewPart(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) createPart(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Creation form for a single part")
+func (app *application) createPart(w http.ResponseWriter, _ *http.Request) {
+	files := []string{
+		"./../../ui/html/base.html",
+		"./../../ui/html/partials/nav.html",
+		"./../../ui/html/pages/create.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	// fmt.Fprintf(w, "Creation form for a single part")
 }
